522d185d3853a1d5528e919de735699699e23354
gabrielsb18 <gbmodelator@gmail.com>
2025-01-27 17:07:56 -0300

    refactor: modificar o contexto de metricas para utilizar o token fornecido pelo AuthContext, e adicionar ele como dependencia do useEffect

--- src/contexts/MetricsContext.jsx
+++ src/contexts/MetricsContext.jsx
@@ -1,40 +1,42 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import { getMetrics } from "../services/metricsService";
+import { useAuth } from "./AuthContext";
 
 export const MetricsContext = createContext({});
 
 function MetricsProvider({ children }) {
+	const { acessToken } = useAuth();
+
 	const [totalTasks, setTotalTasks] = useState({});
 	const [loading, setLoading] = useState(false);
-	
+
 	useEffect(() => {
-		const acessToken = localStorage.getItem("@Notes:token");
 		if (acessToken) {
 			const getTotalPendingTasks = async () => {
 				setLoading(true);
 				try {
-					const response = await getMetrics();
-					const {pendente, concluida} = response;
-					setTotalTasks(prevstate => ({
+					const response = await getMetrics(acessToken);
+					const { pendente, concluida } = response.data;
+					setTotalTasks((prevstate) => ({
 						...prevstate,
 						pendente,
-						concluida
+						concluida,
 					}));
 				} catch (error) {
 					return error.message;
 				} finally {
-					setLoading(false)
+					setLoading(false);
 				}
 			};
 			getTotalPendingTasks();
 		}
-	}, []);
+	}, [acessToken]);
 
 	const context = {
 		totalTasksPending: totalTasks.pendente,
 		totalTasksCompleted: totalTasks.concluida,
-		loading
-	}	
+		loading,
+	};
 
 	return (
 		<MetricsContext.Provider value={context}>
