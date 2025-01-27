b903bfda989c665185c8b3a56cef0c93186db587
gabrielsb18 <gbmodelator@gmail.com>
2025-01-27 17:17:50 -0300

    refactor: ajustar a função Button para exportação nomeada

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,12 @@
-import { ConteinerButton } from "./Button.styles"
+import { ConteinerButton } from "./Button.styles";
 
 /* eslint-disable react/prop-types */
-function Button ({estilo="flat", name, onClick}) {
-    return (
-        <ConteinerButton className={estilo} onClick={onClick} >
-            {name}
-        </ConteinerButton>
-    );
+function Button({ estilo = "flat", name, onClick }) {
+	return (
+		<ConteinerButton className={estilo} onClick={onClick}>
+			{name}
+		</ConteinerButton>
+	);
 }
 
-export default Button
+export { Button };
