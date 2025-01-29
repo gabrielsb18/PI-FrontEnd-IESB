321d79c7f6c994758530d895ea5cd3b3c9937fcc
gabrielsb18 <gbmodelator@gmail.com>
2025-01-28 23:52:05 -0300

    feat: adicionar acesso a landing page para usuarios autenticados

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -2,15 +2,13 @@ import {Route, Routes} from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Cadastro from '../pages/Cadastro/Cadastro';
 import Error404 from '../pages/Error404/Error404';
-import Lpage from "../pages/Lpage/Lpage"
 
 export function AuthRoutes(){
     return(
-    <Routes>
-        <Route path="/" element={<Lpage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="*" element = {<Error404 redirect="/login"/>} />
-    </Routes >
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="*" element = {<Error404 redirect="/login"/>} />
+        </Routes >
     )
 }
