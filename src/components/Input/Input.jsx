e716b34e22c17fc57023d16b8feac18f0d9bbb04
gabrielsb18 <gbmodelator@gmail.com>
2025-03-21 22:56:15 -0300

    style: ajustando identação de codigo

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,17 +1,19 @@
 import { InputWrap, ErrorMessage } from "./Input.styles";
 import React from "react";
 
-const Input = React.forwardRef(({ error, nome, icon:Icon, variant = "flat", ...rest },ref) => {
-    return (
-        <>
-            <label>{nome}</label>
-            <InputWrap variant={variant}>
-                {Icon && <Icon size={20} />}
-                <input {...rest} ref={ref}/>
-            </InputWrap>
-            {error && <ErrorMessage>{error}</ErrorMessage>}
-        </>
-    );
-})
+const Input = React.forwardRef(
+	({ error, nome, icon: Icon, variant = "flat", ...rest }, ref) => {
+		return (
+			<>
+				<label>{nome}</label>
+				<InputWrap variant={variant}>
+					{Icon && <Icon size={20} />}
+					<input {...rest} ref={ref} />
+				</InputWrap>
+				{error && <ErrorMessage>{error}</ErrorMessage>}
+			</>
+		);
+	},
+);
 
-export {Input};
+export { Input };
