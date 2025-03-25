bf9c41ae79e3810809e425bf7403396eb5362f35
gabrielsb18 <gbmodelator@gmail.com>
2025-03-24 22:28:10 -0300

    refactor: atualizar mensagem de erro esperado, em caso de falha de conexão

--- src/services/metricsService.js
+++ src/services/metricsService.js
@@ -3,7 +3,7 @@ import { api } from "./api";
 function getTotalTasks(credentials) {
     return api.get("/notes/totals", {
         headers: {
-            authorization: `Bearer ${ credentials }`
+            authorization: `Bearer ${credentials}`
         }
     })
         .then((response) => {
@@ -17,17 +17,15 @@ function getTotalTasks(credentials) {
         });
 }
 
-function getTotalTasksCompletedOfWeek(){
+function getTotalTasksCompletedOfWeek() {
     return api.get("/notes/notesofweek")
-    .then((response)=>{
-        return { sucess: true, data: response.data}
-    }).catch((error)=> {
-        if(error.response){
-            return { sucess: false, msg: "Notas da semana carregadas com sucesso"}
-        } else {
-            return {sucess: false, msg: "Erro ao carregar o total de notas da semana"}
-        }
-    })
+        .then((response) => {
+            return { sucess: true, data: response.data, msg: "Notas da semana carregadas com sucesso" }
+        }).catch((error) => {
+            if (error.response) {
+                return { sucess: false, msg: "Erro ao carregar o total de notas concluídas da semana" }
+            }
+        })
 }
 
 export { getTotalTasks,getTotalTasksCompletedOfWeek };
